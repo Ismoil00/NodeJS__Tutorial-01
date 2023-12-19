07889c9bc57614b009cd2e2613d604f6fffd200e
Ismoil <juraev.abubakr_98@mail.ru>
2023-12-19 12:18:55 +0500

    new commit

--- Z_WebServer/LogEvents.js
+++ Z_WebServer/LogEvents.js
@@ -1,26 +1,22 @@
 const { format } = require("date-fns");
 const { v4: uuid } = require("uuid");
+
 const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 
-const logEvents = async (message) => {
+const logEvents = async (message, logName) => {
   const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
-  console.log(logItem);
+
   try {
-    // if the folder doesn't exist - then we create it first:
     if (!fs.existsSync(path.join(__dirname, "logs"))) {
       await fsPromises.mkdir(path.join(__dirname, "logs"));
     }
 
-    // we append file
-    await fsPromises.appendFile(
-      path.join(__dirname, "logs", "eventLog.txt"),
-      logItem
-    );
+    await fsPromises.appendFile(path.join(__dirname, "logs", logName), logItem);
   } catch (err) {
-    console.error(err);
+    console.log(err);
   }
 };
 
--- Z_WebServer/server.js
+++ Z_WebServer/server.js
@@ -2,14 +2,40 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 const fsPromises = require("fs").promises;
-const logEvents = require("./LogEvents");
+const logEvents = require("./logEvents");
 const EventEmitter = require("events");
-
 class Emitter extends EventEmitter {}
-const emitter = new Emitter();
+const myEmitter = new Emitter();
 const PORT = process.env.PORT || 3500;
 
+myEmitter.on("log", (msg, fileName) => logEvents(msg, fileName));
+
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const rawData = await fsPromises.readFile(
+      filePath,
+      !contentType.includes("image") ? "utf8" : ""
+    );
+    const data =
+      contentType === "application/json" ? JSON.parse(rawData) : rawData;
+    response.writeHead(filePath.includes("404.html") ? 404 : 200, {
+      "Content-Type": contentType,
+    });
+    response.end(
+      contentType === "application/json" ? JSON.stringify(data) : data
+    );
+  } catch (err) {
+    console.log(err);
+    myEmitter.emit("log", `${err.name}: ${err.message}`, "errLog.txt");
+    response.statusCode = 500;
+    response.end();
+  }
+};
+
 const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  myEmitter.emit("log", `${req.url}\t${req.method}`, "reqLog.txt");
+
   const extension = path.extname(req.url);
   let contentType;
 
@@ -44,12 +70,26 @@ const server = http.createServer((req, res) => {
       : contentType === "text/html"
       ? path.join(__dirname, "views", req.url)
       : path.join(__dirname, req.url);
+
+  // makes .html extension not required in the browser
+  if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
+
+  if (fs.existsSync(filePath)) {
+    serveFile(filePath, contentType, res);
+  } else {
+    switch (path.parse(filePath).base) {
+      case "old-page.html":
+        res.writeHead(301, { Location: "/new-page.html" });
+        res.end();
+        break;
+      case "www-page.html":
+        res.writeHead(301, { Location: "/" });
+        res.end();
+        break;
+      default:
+        serveFile(path.join(__dirname, "views", "404.html"), "text/html", res);
+    }
+  }
 });
 
-server.listen(PORT, () => console.log(`Server listening on ${PORT}`));
-
-// // add listener for the event
-// emitter.on("log", (msg) => logEvents(msg));
-
-// // Emit event
-// myEmitter.emit("log", "Log event emitted!");
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
